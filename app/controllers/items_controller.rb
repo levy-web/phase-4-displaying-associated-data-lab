98f33fcf21d91fcd62ec21c7ca650c37a2355f50
levy <lnjiru43@gmail.com>
2023-03-18 00:28:05 +0300

    completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,9 @@
 class ItemsController < ApplicationController
+
+    def index
+        items = Item.all
+        render json: items, only: [:id, :name, :description, :price], include: :user
+    end
+
+
 end
